eb452345313bc59c6a0588249f67952c8c65bf41
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-05-27 20:08:27 -0300

    Modularizado subir fotos de perfil.

--- index.php
+++ index.php
@@ -29,11 +29,18 @@
         <?php include_once 'footer.php' ?>
     </footer>
     <?php
-        if(isset($_FILES["Foto"])){
-            if($_FILES["Foto"]["error"] === UPLOAD_ERR_OK){
-                move_uploaded_file($_FILES["Foto"]["tmp_name"], dirname(__FILE__)."/img/foto/"."idUsuario".".".pathinfo($_FILES["Foto"]["name"], PATHINFO_EXTENSION));
+        function subirFoto(){
+            if(isset($_FILES["Foto"])){
+                if($_FILES["Foto"]["error"] === UPLOAD_ERR_OK){
+                    move_uploaded_file($_FILES["Foto"]["tmp_name"], dirname(__FILE__)."/img/foto/"."idUsuario".".".pathinfo($_FILES["Foto"]["name"], PATHINFO_EXTENSION));
+                    return true;
+                }
             }
+            return false;
         }
+
+        subirFoto();
+
     ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
