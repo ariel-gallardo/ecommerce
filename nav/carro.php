204b26734fca926b324591bdc260bcd124702017
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-05-27 19:28:22 -0300

    Modularización del carro de compras

---
+++ nav/carro.php
@@ -0,0 +1,59 @@
+<!-- Elementos del carro de compras -->
+<ul class="list-group p-0">
+    <li class="list-group-item border-0 text-left py-0">
+        <p><span class="mx-2 badge badge-secondary p-0">x3</span>Auriculares Inalambr.<span
+                class="p-0 badge mx-1">600$ARS</span><svg class="Quitar mx-1" xmlns="http://www.w3.org/2000/svg"
+                width="8" height="8" viewBox="0 0 8 8">
+                <path
+                    d="M1.41 0l-1.41 1.41.72.72 1.78 1.81-1.78 1.78-.72.69 1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72-1.44-1.41-.69.72-1.78 1.78-1.81-1.78-.72-.72z" />
+            </svg></span></p>
+    </li>
+    <li class="list-group-item border-0 text-left py-0">
+        <p><span class="mx-2 badge badge-secondary p-0">x2</span>Auriculares Cableados<span
+                class="p-0 badge mx-1">200$ARS</span><svg class="Quitar mx-1" xmlns="http://www.w3.org/2000/svg"
+                width="8" height="8" viewBox="0 0 8 8">
+                <path
+                    d="M1.41 0l-1.41 1.41.72.72 1.78 1.81-1.78 1.78-.72.69 1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72-1.44-1.41-.69.72-1.78 1.78-1.81-1.78-.72-.72z" />
+            </svg></span></p>
+    </li>
+    <li class="list-group-item border-0 text-left py-0">
+        <p><span class="mx-2 badge badge-secondary p-0">x3</span>Perfume Savage Dior<span
+                class="p-0 badge mx-1">200$ARS</span><svg class="Quitar mx-1" xmlns="http://www.w3.org/2000/svg"
+                width="8" height="8" viewBox="0 0 8 8">
+                <path
+                    d="M1.41 0l-1.41 1.41.72.72 1.78 1.81-1.78 1.78-.72.69 1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72-1.44-1.41-.69.72-1.78 1.78-1.81-1.78-.72-.72z" />
+            </svg></span></p>
+    </li>
+    <li class="list-group-item border-0 text-left py-0">
+        <p><span class="mx-2 badge badge-secondary p-0">x4</span>Perfume Channel<span
+                class="p-0 badge mx-1">100$ARS</span><svg class="Quitar mx-1" xmlns="http://www.w3.org/2000/svg"
+                width="8" height="8" viewBox="0 0 8 8">
+                <path
+                    d="M1.41 0l-1.41 1.41.72.72 1.78 1.81-1.78 1.78-.72.69 1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72-1.44-1.41-.69.72-1.78 1.78-1.81-1.78-.72-.72z" />
+            </svg></span></p>
+    </li>
+    <li class="list-group-item border-0 text-left py-0">
+        <p><span class="mx-2 badge badge-secondary p-0">x5</span>Perfume Biografia<span
+                class="p-0 badge mx-1">25$ARS</span><svg class="Quitar mx-1" xmlns="http://www.w3.org/2000/svg"
+                width="8" height="8" viewBox="0 0 8 8">
+                <path
+                    d="M1.41 0l-1.41 1.41.72.72 1.78 1.81-1.78 1.78-.72.69 1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72-1.44-1.41-.69.72-1.78 1.78-1.81-1.78-.72-.72z" />
+            </svg></span></p>
+    </li>
+    <li class="list-group-item border-0 text-left py-0">
+        <p><span class="mx-2 badge badge-secondary p-0">x3</span>Destornillador Philip<span
+                class="p-0 badge mx-1">200$ARS</span><svg class="Quitar mx-1" xmlns="http://www.w3.org/2000/svg"
+                width="8" height="8" viewBox="0 0 8 8">
+                <path
+                    d="M1.41 0l-1.41 1.41.72.72 1.78 1.81-1.78 1.78-.72.69 1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72-1.44-1.41-.69.72-1.78 1.78-1.81-1.78-.72-.72z" />
+            </svg></span></p>
+    </li>
+    <li class="list-group-item border-0 text-left py-0">
+        <p><span class="mx-2 badge badge-secondary p-0">x3</span>Destornillador Plano<span
+                class="p-0 badge mx-1">200$ARS</span><svg class="Quitar mx-1" xmlns="http://www.w3.org/2000/svg"
+                width="8" height="8" viewBox="0 0 8 8">
+                <path
+                    d="M1.41 0l-1.41 1.41.72.72 1.78 1.81-1.78 1.78-.72.69 1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72-1.44-1.41-.69.72-1.78 1.78-1.81-1.78-.72-.72z" />
+            </svg></span></p>
+    </li>
+</ul>
