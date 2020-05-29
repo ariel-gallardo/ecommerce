13ce1e8396c6c95d30c631a434a844a163f0ef57
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-05-29 00:44:00 -0300

    Optimizando sección del usuario.

--- nav/usuario/usuario.php
+++ nav/usuario/usuario.php
@@ -1,4 +1,13 @@
-<?php include_once 'usuarioDAO.php' ?>
+<?php
+    include_once 'usuarioDAO.php';
+
+    $campos = [
+        "Perfil" => true,
+        "Login" => false,
+        "Registro" =>false
+    ];
+
+?>
 <!-- Seccion del Usuario -->
 <div class="text-center" data-toggle="modal" data-target="#Usuario">
     <img src="/img/nav/usuario.svg" alt="Perfil" width="35vw">
@@ -12,40 +21,23 @@
         <div class="modal-body">
             <div class="modal-c-tabs">
                 <ul class="nav nav-tabs md-tabs tabs-2" role="tablist">
-                    <?php if(isset($_SESSION["Usuario"])):?>
-                    <!-- Tabs del Usuario -->
-                    <!-- Perfil de Usuario -->
-                    <li class="nav-item">
-                        <a class="nav-link text-center" data-toggle="tab" role="tab" href="#Perfil">
-                            Perfil
-                        </a>
-                    </li>
-                    <?php else: ?>
-                    <!-- Tabulacion de Usuario -->
-                    <li class="nav-item">
-                        <a class="nav-link text-center" data-toggle="tab" role="tab" href="#Login">
-                            Login
-                        </a>
-                    </li>
-                    <!-- Tabulacion de registro -->
-                    <li class="nav-item">
-                        <a class="nav-link text-center" data-toggle="tab" role="tab" href="#Registro">
-                            Registro
-                        </a>
-                    </li>
-                    <?php endif; ?>
+                    <?php foreach($campos as $campo => $logueado): ?>
+                        <?php if((isset($_SESSION["Usuario"]) && $logueado) || (!isset($_SESSION["Usuario"]) && !$logueado)) :?>
+                        <li class="nav-item">
+                            <a class="nav-link text-center" data-toggle="tab" role="tab" href="#<?=$campo?>">
+                                <?=$campo?>
+                            </a>
+                        </li>
+                        <?php endif;?>
+                    <?php endforeach; ?>
                 </ul>
             <div class="tab-content">  
-            <?php if(isset($_SESSION["Usuario"])): ?> 
-            <!-- Perfil del Usuario -->
-                <?php include "perfil.php" ?>
-            <?php else: ?>
-                <!-- Formulario de Login -->
-                    <?php include 'login.php' ?>
-                <!-- Formulario de Registro -->
-                    <?php include 'registro.php' ?>
-                <?php endif; ?>
-            </div>
+
+            <?php foreach($campos as $campo => $logueado): ?>
+                <?php if((isset($_SESSION["Usuario"]) && $logueado) || (!isset($_SESSION["Usuario"]) && !$logueado)) :?>
+                    <?php include $campo.'.php' ?>
+                <?php endif;?>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
