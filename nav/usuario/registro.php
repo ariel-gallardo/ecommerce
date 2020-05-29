924fd45f1efc90cb0068a45396aacd9b3f9549cc
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-05-29 01:22:15 -0300

    Optimizado formulario registro

--- nav/usuario/registro.php
+++ nav/usuario/registro.php
@@ -1,16 +1,22 @@
+<?php
+    $campos = [
+        "Nombre",
+        "Apellido",
+        "Provincia",
+        "Localidad",
+        "Calle",
+        "Correo",
+        "Password",
+        "Password_Verify"
+    ];
+?>
+
 <!-- Formulario de Registro -->
 <div class="tab-pane fade in show" id="Registro" role="tabpanel">
     <form action="." method="POST" enctype = "multipart/form-data">
-        <input class = "form-control my-2" type = "text" name = "Nombre"  placeholder = "Nombre/s" required>
-        <input class = "form-control my-2" type = "text" name = "Apellido"  placeholder = "Apellido/s" required>
-        <!-- Provincias ordenadas por codigo -->
-        <input class = "form-control my-2" name = "Provincia" placeholder = "Provincia" required>
-        <input class = "form-control my-2" name = "Localidad" placeholder = "Localidad" required>
-        <input class = "form-control my-2" type = "text" name = "Calle" placeholder = "Calle" required>
-        <input class = "form-control my-2" type = "email" name = "Correo" placeholder = "Correo Electrónico" required>
-        <input class = "form-control my-2" type = "password" name = "Password" placeholder = "Contraseña" required>
-        <input class = "form-control my-2" type = "password" name = "Password_Verify" placeholder = "Repetir Contraseña"
-            required>
+        <?php foreach($campos as $campo) : ?>
+            <input class = "form-control my-2" type = "<?= ($campo == 'Password' || $campo == 'Password_Verify' ? 'password' : ($campo == 'Correo' ? 'email' : 'text')) ?>" name = "<?=$campo?>"  placeholder = "<?= ($campo == 'Password' || $campo == 'Password_Verify' ? 'Contraseña' : ($campo == 'Correo' ? 'Correo electrónico' : $campo)) ?>" required>
+        <?php endforeach; ?>
         <div class = "my-2">
             <span><b>Foto de Perfil</b></span>
             <input class = "border-0" type = "file" name = "Foto" accept = "image/*" required>
